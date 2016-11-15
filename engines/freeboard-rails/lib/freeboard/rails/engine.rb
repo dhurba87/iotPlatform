882c4c1c1d7ae81cbfbabed1a5667180076ff56f
Dhurba Baral <dhurba87@gmail.com>
2016-11-15 15:27:16 +0545

    add freeboard dashboard page

--- engines/freeboard-rails/lib/freeboard/rails/engine.rb
+++ engines/freeboard-rails/lib/freeboard/rails/engine.rb
@@ -1,6 +1,12 @@
 module Freeboard
   module Rails
     class Engine < ::Rails::Engine
+      config.local_public_freeboard_path = File.join(root, "public", "freeboard").to_s
+
+      # Initializer to combine this engines static assets with the static assets of the hosting site.
+      initializer "static assets" do |app|
+        app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+      end
     end
   end
 end
